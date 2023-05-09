fd29cb441ddfcb8d3eb2f0f6bf4d760afa7b6521
BotBw <wang1570@e.ntu.edu.sg>
2023-05-09 21:43:39 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import os 
 
 os.system("pip install resources/timm-0.1.26-py3-none-any.whl")
-os.system("pip install pycocotools")
 os.system("pip install -r resources/yolov5/requirements.txt")
 os.system("pip install resources/efficientdet/requirements.txt")
-os.system("python resources/Weighted-Boxes-Fusion/setup.py")
 os.system("python resources/omegaconf/setup.py")
-
-
+os.system("python resources/Weighted-Boxes-Fusion/setup.py")
