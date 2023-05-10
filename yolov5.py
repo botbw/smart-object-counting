d6708150f9813962b29ff2f135e9f97c8a8840b1
BotBw <wang1570@e.ntu.edu.sg>
2023-05-10 12:57:04 +0800

    add yolov5

--- yolov5.py
+++ yolov5.py
@@ -26,12 +26,12 @@ def train_yolov5(marking, fold):
     val_index = index[len(index) * fold // 5:len(index) * (fold + 1) // 5]
     for name, mini in tqdm(marking.groupby('image_id')):
         if name in val_index:
-            path2save = CACHE_DIR + 'val2017/'
+            path2save = 'val2017/'
         else:
-            path2save = CACHE_DIR + 'train2017/'
-        if not os.path.exists(CACHE_DIR + 'convertor/fold{}/labels/'.format(fold) + path2save):
-            os.makedirs(CACHE_DIR + 'convertor/fold{}/labels/'.format(fold) + path2save)
-        with open(CACHE_DIR + 'convertor/fold{}/labels/'.format(fold) + path2save + name + ".txt", 'w+') as f:
+            path2save = 'train2017/'
+        if not os.path.exists('convertor/fold{}/labels/'.format(fold) + path2save):
+            os.makedirs('convertor/fold{}/labels/'.format(fold) + path2save)
+        with open('convertor/fold{}/labels/'.format(fold) + path2save + name + ".txt", 'w+') as f:
             row = mini[['classes','x_center','y_center','w','h']].astype(float).values
             row = row/1024
             row = row.astype(str)
@@ -39,60 +39,12 @@ def train_yolov5(marking, fold):
                 text = ' '.join(row[j])
                 f.write(text)
                 f.write("\n")
-        if not os.path.exists(CACHE_DIR + 'convertor/fold{}/images/{}'.format(fold, path2save)):
-            os.makedirs(CACHE_DIR + 'convertor/fold{}/images/{}'.format(fold, path2save))
+        if not os.path.exists('convertor/fold{}/images/{}'.format(fold, path2save)):
+            os.makedirs('convertor/fold{}/images/{}'.format(fold, path2save))
         sh.copy(DATA_DIR + "{}/{}.jpg".format(source,name),'convertor/fold{}/images/{}/{}.jpg'.format(fold, path2save, name))
-
-    with open('yolov5x.yaml') as f:
-        f.write(
-"""\
-# parameters
-nc: 1  # number of classes
-depth_multiple: 1.33  # model depth multiple
-width_multiple: 1.25  # layer channel multiple
-
-# anchors
-anchors:
-  - [10,13, 16,30, 33,23]  # P3/8
-  - [30,61, 62,45, 59,119]  # P4/16
-  - [116,90, 156,198, 373,326]  # P5/32
-
-# yolov5 backbone
-backbone:
-  # [from, number, module, args]
-  [[-1, 1, Focus, [64, 3]],  # 1-P1/2
-   [-1, 1, Conv, [128, 3, 2]],  # 2-P2/4
-   [-1, 3, Bottleneck, [128]],
-   [-1, 1, Conv, [256, 3, 2]],  # 4-P3/8
-   [-1, 9, BottleneckCSP, [256]],
-   [-1, 1, Conv, [512, 3, 2]],  # 6-P4/16
-   [-1, 9, BottleneckCSP, [512]],
-   [-1, 1, Conv, [1024, 3, 2]], # 8-P5/32
-   [-1, 1, SPP, [1024, [5, 9, 13]]],
-   [-1, 6, BottleneckCSP, [1024]],  # 10
-  ]
-
-# yolov5 head
-head:
-  [[-1, 3, BottleneckCSP, [1024, False]],  # 11
-   [-1, 1, nn.Conv2d, [na * (nc + 5), 1, 1]],  # 12 (P5/32-large)
-
-   [-2, 1, nn.Upsample, [None, 2, 'nearest']],
-   [[-1, 6], 1, Concat, [1]],  # cat backbone P4
-   [-1, 1, Conv, [512, 1, 1]],
-   [-1, 3, BottleneckCSP, [512, False]],
-   [-1, 1, nn.Conv2d, [na * (nc + 5), 1, 1]],  # 17 (P4/16-medium)
-
-   [-2, 1, nn.Upsample, [None, 2, 'nearest']],
-   [[-1, 4], 1, Concat, [1]],  # cat backbone P3
-   [-1, 1, Conv, [256, 1, 1]],
-   [-1, 3, BottleneckCSP, [256, False]],
-   [-1, 1, nn.Conv2d, [na * (nc + 5), 1, 1]],  # 22 (P3/8-small)
-
-   [[], 1, Detect, [nc, anchors]],  # Detect(P3, P4, P5)
-  ]
-"""
-        )
+    
+    # move convertor to yolov5 folder
+    sh.move('convertor', 'resources/yolov5')
 
     with open('dataset.yaml', 'w') as f:
         f.write(
@@ -117,7 +69,18 @@ nc: 1
 names: ['wheat']
 """
         )
-    subprocess.call(f"python resourses/yolov5/train.py --img 1024 --batch 2 --epochs 50 --data dataset.yaml --cfg yolov5x.yaml --name yolov5x")
+    device = "0" if torch.cuda.is_available() else "cpu"
+    subprocess.call(f"\
+                    python resources/yolov5/train.py \
+                    --data dataset.yaml \
+                    --cfg resources/yolov5/models/yolov5x.yaml \
+                    --weights resources/yolov5/yolov5x.pt \
+                    --epochs 50 \
+                    --img 1024 \
+                    --batch -1 \
+                    --device {device} \
+                    --optimizer SGD \
+                    ")
 
 
 def detectSingle(im0, imgsz, model, device, conf_thres, iou_thres):
